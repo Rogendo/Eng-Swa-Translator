14262d19c80acc697e5b3e496577be9cfb25e40c
Peter <62094358+Rogendo@users.noreply.github.com>
2023-11-04 05:56:27 +0300

    Update translator.py

--- deployment/translator.py
+++ deployment/translator.py
@@ -1,18 +1,61 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 
-model_checkpoint = "Helsinki-NLP/opus-mt-en-swc"
-tokenizer = AutoTokenizer.from_pretrained("../model/")
-model = AutoModelForSeq2SeqLM.from_pretrained("../model/")
+# translator pipeline for english to swahili translations
+eng_swa_model_checkpoint = "Helsinki-NLP/opus-mt-en-swc"
+eng_swa_tokenizer = AutoTokenizer.from_pretrained("../model/eng_swa_model/")
+eng_swa_model = AutoModelForSeq2SeqLM.from_pretrained("../model/eng_swa_model/")
 
-translator = pipeline(
+eng_swa_translator = pipeline(
     "text2text-generation",
-    model=model,
-    tokenizer=tokenizer,
+    model=eng_swa_model,
+    tokenizer=eng_swa_tokenizer,
 )
 
-def translate_text(text):
-    translated_text = translator(text, max_length=128, num_beams=5)[0]['generated_text']
+def translate_text_eng_swa(text):
+    translated_text = eng_swa_translator(text, max_length=128, num_beams=5)[0]['generated_text']
     return translated_text
+
+# translator pipeline for swahili to english translations
+swa_eng_model_checkpoint = "Helsinki-NLP/opus-mt-swc-en"
+swa_eng_tokenizer = AutoTokenizer.from_pretrained("../model/swa_eng_model/")
+swa_eng_model = AutoModelForSeq2SeqLM.from_pretrained("../model/swa_eng_model/")
+
+swa_eng_translator = pipeline(
+    "text2text-generation",
+    model=swa_eng_model,
+    tokenizer=swa_eng_tokenizer,
+)
+
+def translate_text_swa_eng(text):
+    translated_text = swa_eng_translator(text, max_length=128, num_beams=5)[0]['generated_text']
+    return translated_text
+
+
+
+# import pickle
+# from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+# # Replace 'your_model.pkl' with the path to your .pkl model file
+# with open('../model/model.pkl', 'rb') as file:
+#     loaded_model = pickle. load(file)
+    
+# class CustomSeq2SeqModel:
+#     def __init__(self, model):
+#         self.model = model
+
+#     def generate(self, input_text, max_length, num_beams):
+#         return self.model.generate(input_text, max_length=max_length, num_beams=num_beams, no_repeat_ngram_size=2)
+
+# # Create an instance of the custom model class
+# custom_model = CustomSeq2SeqModel(loaded_model)
+# tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-swc")
+# translator = pipeline(
+#     "text2text-generation",
+#     model=custom_model,
+#     tokenizer=tokenizer,
+# )
+# def translate_text(text):
+#     translated_text = translator(text, max_length=128, num_beams=5)[0]['generated_text']
+#     return translated_text
 
 # text="he is a big man, he can make his own choices"
 # translate_text(text)
