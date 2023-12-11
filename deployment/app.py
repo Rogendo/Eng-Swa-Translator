902b9e13642a9fd20e2dd0915dd723850e4d60ce
Rogendo <peterrogendo1999@gmail.com>
2023-12-11 19:59:44 +0300

    updated ui

--- deployment/app.py
+++ deployment/app.py
@@ -17,16 +17,32 @@ def home():
 def chat():
     return render_template('chat.html')
 
+
+@app.route('/chat2', methods=['GET', 'POST'])
+def chat2():
+    return render_template('chat2.html')
+
 @socketio.on('message')
 def handle_message(message):
-    # translated_text = translate_text_eng_swa(message)
-    translated_text = translate_text_swa_eng(message)
+    translated_text = translate_text_eng_swa(message)
+    # translated_text = translate_text_swa_eng(message)
 
     data = {
         'original': message,
         'translated': translated_text
     }
     emit('message', data, broadcast=True)
+    
+@socketio.on('eng_message')
+def handle_eng_message(message):
+    translated_text = translate_text_swa_eng(message)
+
+    data = {
+        'original': message,
+        'translated': translated_text
+    }
+    emit('eng_message', data, broadcast=True)
+
 
 if __name__ == '__main__':
     socketio.run(app)
